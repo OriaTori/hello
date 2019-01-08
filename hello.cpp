f76eb52433547e0706c7d73aa7a22d6042ce25a0
Martyna Łęcka <martynalecka.ml@gmail.com>
2019-01-08 18:22:27 +0100

    Third version - added line with information about git checkout

--- hello.cpp
+++ hello.cpp
@@ -4,5 +4,6 @@ int main(){
 
     std::cout<<"Hello world\n";
     std::cout<<"Remamber - better version with endl"<< std::endl;
+    std::cout<<"To change branch use: git checkout\n";
     return 0;
 }
