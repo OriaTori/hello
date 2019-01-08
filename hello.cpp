6bafa95fb8597479f28f3f9c3ee781b73ae40aa0
Martyna Łęcka <martynalecka.ml@gmail.com>
2019-01-08 19:23:42 +0100

    Changes in line

--- hello.cpp
+++ hello.cpp
@@ -5,6 +5,6 @@ int main(){
     std::cout<<"Hello world\n";
     std::cout<<"Remamber - better version with endl"<< std::endl;
     std::cout<<"To change branch use: git checkout\n";
-    std::cout<<"Change something to make a commit";
+    std::cout<<"Change something to make a commit - on different branch";
     return 0;
 }
