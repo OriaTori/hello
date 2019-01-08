759dcbae4d0b7128962fb7f82a96d58af1fa19ce
Martyna Łęcka <martynalecka.ml@gmail.com>
2019-01-08 19:14:17 +0100

    Simple change - added sentence

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 
 int main(){
-
+    
     std::cout<<"Hello world\n";
     std::cout<<"Remamber - better version with endl"<< std::endl;
     std::cout<<"To change branch use: git checkout\n";
+    std::cout<<"Change something to make a commit";
     return 0;
 }
