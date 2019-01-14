f834e05f1303a821ba1fb3a87b3298833ef8acf0
Martyna Łęcka <martynalecka.ml@gmail.com>
2019-01-14 14:12:49 +0100

    Changes in file hello.cpp: remove lines with array

--- hello.cpp
+++ hello.cpp
@@ -1,24 +1,11 @@
 #include <iostream>
-#include <random>
 
 int main(){
-    // to storage array in heap
-    int* tab = new int[37];
-    
-    // Random number
-    std::random_device r;
-    std::default_random_engine e1(r());
-    std::uniform_int_distribution<int> uniform_dist(1,255);
 
-    for(int i=0; i < 37; i++){
-        tab[i] = uniform_dist(e1);
-    }
+    std::cout<<"Hello Martyna"<<std::endl;
+    std::cout<<"Remember - better version with endl"<< std::endl;
+    std::cout<<"To change branch use: git checkout"<<std::endl;
+    std::cout<<"Change something to make a commit - on a different branch"<< std::endl;
 
-    std::cout<<"Hello Martyna\n";
-    std::cout<<"Remamber - better version with endl"<< std::endl;
-    std::cout<<"To change branch use: git checkout\n";
-    std::cout<<"Change something to make a commit - on different branch"<< std::endl;
-
-    delete[] tab;
     return 0;
 }
